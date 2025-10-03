4710663f151986818394d72d4a3892948ace0d75
Android Studio <devandroid@ort>
2025-10-03 10:22:48 -0300

    Agregado Ionicons al Tab navegation del proyecto

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 
 import WalkScreen from "./src/screens/WalkScreen";
 import MapScreen from "./src/screens/MapScreen";
@@ -21,10 +22,26 @@ export default function App() {
             tabBarInactiveTintColor: 'gray',
           }}
         >
-          <Tab.Screen name="Caminar" component={WalkScreen} />
-          <Tab.Screen name="Mapa" component={MapScreen} />
-          <Tab.Screen name="Notificaciones" component={NotificationScreen} />
-          <Tab.Screen name="Música" component={MusicScreen} />
+          <Tab.Screen name="Caminar" component={WalkScreen} options={{
+              tabBarIcon: ({ color }) => (
+                <Ionicons name="walk" size={24} color={color} />
+              ),
+            }}/>
+          <Tab.Screen name="Mapa" component={MapScreen} options={{
+              tabBarIcon: ({ color }) => (
+                <Ionicons name="map" size={24} color={color} />
+              ),
+            }}/>
+          <Tab.Screen name="Notificaciones" component={NotificationScreen} options={{
+              tabBarIcon: ({ color }) => (
+                <Ionicons name="notifications" size={24} color={color} />
+              ),
+            }}/>
+          <Tab.Screen name="Música" component={MusicScreen} options={{
+              tabBarIcon: ({ color }) => (
+                <Ionicons name="musical-notes" size={24} color={color} />
+              ),
+            }}/>
         </Tab.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
